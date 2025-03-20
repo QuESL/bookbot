196ac84a8291cd920ef54d33cc2e920c51399b9e
QuESL <quentin.esl@outlook.com>
2025-03-20 18:21:07 +0100

    initiated bookbot

---
+++ main.py
@@ -0,0 +1,13 @@
+from stats import get_num_words
+
+def get_book_text(path):
+    with open(path) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    book_text = get_book_text("books/frankenstein.txt")
+    word_count = get_num_words(book_text)
+    print(f"{word_count} words found in the document")
+
+main()
---
+++ stats.py
@@ -0,0 +1,6 @@
+def get_num_words(book_text):
+    return len(book_text.split())
+
+def get_num_characters(book_text):
+    book_text = book_text.lower()
+    
