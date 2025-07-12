ce6dd0870e1e2fd9871df06494cd2c44118cf028
QuESL <quentin.esl@outlook.com>
2025-07-12 15:53:23 +0200

    nearing completion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import get_num_words
 from stats import get_num_characters
+from stats import get_sorted_list_of_dict
 
 def get_book_text(path):
     with open(path) as f:
@@ -10,10 +11,26 @@ def main():
     book_text = get_book_text("books/frankenstein.txt")
 
     word_count = get_num_words(book_text)
-    print(f"{word_count} words found in the document")
+    # print(f"{word_count} words found in the document")
 
     character_count = get_num_characters(book_text)
-    for char, count in character_count.items():
-        print(f"'{char}': {count}")
+    # for char, count in character_count.items():
+    #     print(f"'{char}': {count}")
 
+    print(f"""
+        ============ BOOKBOT ============
+        Analyzing book found at {book_text}...
+        ----------- Word Count ----------
+        Found {word_count} total words
+        --------- Character Count -------
+    """)
+
+    sorted_list_of_dict = get_sorted_list_of_dict(character_count)
+    for item in sorted_list_of_dict:
+        char = item["char"]
+        num = item["num"]
+        if char.isalpha():
+            print(f"{char}: {num}")
+    print("============= END ===============")
+    
 main()
--- stats.py
+++ stats.py
@@ -10,3 +10,12 @@ def get_num_characters(book_text):
         else:
             counts[character] = 1
     return counts
+
+def get_sorted_list_of_dict(character_dictionary):
+    list_of_dict = list()
+    for key, value in character_dictionary.items():
+        new_sub_dict = {"char": key, "num": value}
+        list_of_dict.append(new_sub_dict)
+    list_of_dict.sort(reverse=True, key=lambda x: x["num"])
+    return list_of_dict
+
