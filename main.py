b53d1d482aed94bdb4fdd805ff8f647b11f299c2
QuESL <quentin.esl@outlook.com>
2025-07-12 15:15:03 +0200

    getting back to finish the bookbot project

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words
+from stats import get_num_characters
 
 def get_book_text(path):
     with open(path) as f:
@@ -7,7 +8,12 @@ def get_book_text(path):
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
+
     word_count = get_num_words(book_text)
     print(f"{word_count} words found in the document")
+
+    character_count = get_num_characters(book_text)
+    for char, count in character_count.items():
+        print(f"'{char}': {count}")
 
 main()
--- stats.py
+++ stats.py
@@ -3,4 +3,10 @@ def get_num_words(book_text):
 
 def get_num_characters(book_text):
     book_text = book_text.lower()
-    
+    counts = {}
+    for character in book_text:
+        if character in counts:
+            counts[character] += 1
+        else:
+            counts[character] = 1
+    return counts
